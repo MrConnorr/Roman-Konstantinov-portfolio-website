5a1207e00c6ebc8c1832ee23b0532b3fbd365ce2
Roman Konstantinov <dragonden2010@gmail.com>
2022-03-03 16:36:04 +0200

    - VN sprites preload and optimization

--- scripts/main.js
+++ scripts/main.js
@@ -39,4 +39,22 @@ function changeNavbar()
 document.addEventListener("DOMContentLoaded", () =>
 {
     changeVNPosition();
+    vnSpritesPreload(
+    "images/sprites/narrator.png",
+    "images/sprites/smile1.png",
+    "images/sprites/smile2.png",
+    "images/sprites/smile3.png",
+    "images/sprites/stressed.png",
+    "images/sprites/surprised.png",
+    "images/sprites/tutorial.png");
 })
+
+
+function vnSpritesPreload()
+{
+    const images = [];
+    for (let i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = vnSpritesPreload.arguments[i];
+    }
+}
